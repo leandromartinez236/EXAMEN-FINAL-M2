7f7cf032f14d2918d97d48be1aa470e2c81b6108
leandro <martinezleadro2004@gmail.com>
2022-01-25 00:45:04 -0300

    terminado

--- main.js
+++ main.js
@@ -10,23 +10,40 @@ signInButton.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
 
+const apiTienda = document.querySelector(".container");
+const url = "https://tiendavirtualmern.herokuapp.com/api";
+fetch(url).then((response) => console.log(response));
 
-const formulario = document.getElementById('formulario')
-formulario.addEventListener('submit', (e) =>{
-  e.preventDefault()
-  let name = document.getElementById('name').value
-  let email = document.getElementById('email').value
-  let password = document.getElementById('password').value
-  // let usuario = {name, email, password}
-  // console.log(usuario); 
-  fetch('https://tiendavirtualmern.herokuapp.com/api/users', {
+
+document.getElementById('formulario').addEventListener('submit', (e)=> {
+  e.preventDefault();
+  const createAccount = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value,
+  }
+  console.log(createAccount);
+  fetch("https://tiendavirtualmern.herokuapp.com/api/users", {
+    headers: { "Content-Type" : "application/json" },
     method: 'POST',
-    body: JSON.stringify({name: 'leandro', email: 'leandro@gmail.com',password: '1234'}),
-    headers: {'Content-Type':'aplication/json'}
+    body: JSON.stringify(createAccount)
   })
   .then((response) => response.json())
-  .then((data)=> console.log(data))
-  .catch((error) => {
-    console.error(error)
-  })
+  .then((data) => console.log(data));
 })
+
+document.getElementById('formulario-dos').addEventListener('submit', (e)=> {
+  e.preventDefault();
+  const signIn = {
+    email: document.getElementById('loginEmail').value,
+    password: document.getElementById('loginPassword').value,
+  }
+  fetch("https://tiendavirtualmern.herokuapp.com/api/users/login", {
+    headers: { "Content-Type" : "application/json" },
+    method: 'POST',
+    body: JSON.stringify(signIn)
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+})
+
